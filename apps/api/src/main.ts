164202dcf9c34eba04c25c538ed7887bd2349639
RLF248 <ralen.flick@pinnacol.com>
2023-02-06 08:26:08 -0700

    back to working state

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,30 +2,29 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
 import * as express from 'express';
 import * as path from 'path';
-import * as fs from 'fs';
-
+import { scheduleData } from './app/api.data';
 const app = express();
-import cors = require('cors');
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api.data.json', (req, res) => {
-  fs.readFile('./data.json', 'utf-8', (err, jsonString) => {
-    if (err) {
-      console.log(err);
-    } else {
-      const scheduleData = JSON.parse(jsonString);
-      res.send(scheduleData);
-    }
-  });
+app.post('/api', (req) => {
+  const { teams, rink, time, address } = req.body;
+  const newData = {
+    teams: teams,
+    rink: rink,
+    time: time,
+    address: address,
+  };
+  scheduleData.push(newData);
 });
+console.log(scheduleData);
 
+app.get('/api', (req, res) => {
+  res.send(scheduleData);
+});
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
